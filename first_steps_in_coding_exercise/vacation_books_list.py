d1486623d1fe871cbe0824500014c6b9b0a04ebb
Nikolay Nikolaev <114243488+NNikolaevvr@users.noreply.github.com>
2023-01-22 15:50:51 +0200

    Delete first_steps_in_coding_exercise directory

--- first_steps_in_coding_exercise/vacation_books_list.py
+++
@@ -1,8 +0,0 @@
-number_of_pages = int(input())
-pages_read_for_1_hour = int(input())
-needed_days = int(input())
-
-total_time_to_read_book = number_of_pages / pages_read_for_1_hour
-needed_hours_per_day = total_time_to_read_book / needed_days
-
-print(int(needed_hours_per_day))
